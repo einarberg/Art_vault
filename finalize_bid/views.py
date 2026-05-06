83365965705bbf434a86f845c70d541fe4cefbfb
einarberg <59967614+einarberg@users.noreply.github.com>
2026-05-06 12:12:30 +0000

    adding finalize_bid app files

---
+++ finalize_bid/views.py
@@ -0,0 +1,8 @@
+from django.core.signals import request_started
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse(f"Response from {request.path}")
