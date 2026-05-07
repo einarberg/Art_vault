1e0d3333717b9a815e629a08bba420c43183cf7b
Freyrgrimsson <129167138+Freyrgrimsson@users.noreply.github.com>
2026-05-07 10:17:24 +0000

    Update models.py

--- finalize_bid/models.py
+++ finalize_bid/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Bid(models.Model):
+    artwork = models.ForeignKey(
+        Artwork,
+        on_delete=models.CASCADE
+    )
+
+    buyer = models.ForeignKey(
+        Buyer,
+        on_delete=models.CASCADE
+    )
+
+    price = models.IntegerField()
+
+    date_of_bid = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    expiration_date = models.DateTimeField()
+
+    status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.buyer} - {self.price}"
