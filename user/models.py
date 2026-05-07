1d206a9d7998e5d83edc13e227c4a5c2f677f258
Freyrgrimsson <129167138+Freyrgrimsson@users.noreply.github.com>
2026-05-07 10:17:52 +0000

    Update models.py

--- user/models.py
+++ user/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    profile_image = models.ImageField(
+        upload_to='profiles/',
+        blank=True,
+        null=True
+    )
+
+    bio = models.TextField(blank=True)
+    cover_image = models.ImageField(
+        upload_to='covers/',
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Buyer(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.user.name
+# Maybe payment information?
+
+
+class Seller(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.user.name
+
+
+class Artist(models.Model):
+    seller = models.OneToOneField(
+        Seller,
+        on_delete=models.CASCADE
+    )
+
+
+class Gallery(models.Model):
+    seller = models.OneToOneField(
+        Seller,
+        on_delete=models.CASCADE
+    )
+
+    street_name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
