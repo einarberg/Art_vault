dee2dfbd761caeeb5d7bcc69016c08dbc6638451
Freyrgrimsson <129167138+Freyrgrimsson@users.noreply.github.com>
2026-05-07 10:17:02 +0000

    Update models.py

--- artwork/models.py
+++ artwork/models.py
@@ -1,66 +1,6 @@
 from django.db import models
 
 
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-
-    profile_image = models.ImageField(
-        upload_to='profiles/',
-        blank=True,
-        null=True
-    )
-
-    bio = models.TextField(blank=True)
-    cover_image = models.ImageField(
-        upload_to='covers/',
-        blank=True,
-        null=True
-    )
-
-    def __str__(self):
-        return self.name
-
-
-class Buyer(models.Model):
-    user = models.OneToOneField(
-        User,
-        on_delete=models.CASCADE
-    )
-
-    def __str__(self):
-        return self.user.name
-# Maybe payment information?
-
-
-class Seller(models.Model):
-    user = models.OneToOneField(
-        User,
-        on_delete=models.CASCADE
-    )
-
-    def __str__(self):
-        return self.user.name
-
-
-class Artist(models.Model):
-    seller = models.OneToOneField(
-        Seller,
-        on_delete=models.CASCADE
-    )
-
-
-class Gallery(models.Model):
-    seller = models.OneToOneField(
-        Seller,
-        on_delete=models.CASCADE
-    )
-
-    street_name = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    postal_code = models.CharField(max_length=20)
-
-
 class Artwork(models.Model):
     seller = models.ForeignKey(
         Seller,
@@ -106,28 +46,3 @@ class Picture(models.Model):
     image = models.ImageField(
         upload_to='artwork_gallery/'
     )
-
-
-class Bid(models.Model):
-    artwork = models.ForeignKey(
-        Artwork,
-        on_delete=models.CASCADE
-    )
-
-    buyer = models.ForeignKey(
-        Buyer,
-        on_delete=models.CASCADE
-    )
-
-    price = models.IntegerField()
-
-    date_of_bid = models.DateTimeField(
-        auto_now_add=True
-    )
-
-    expiration_date = models.DateTimeField()
-
-    status = models.CharField(max_length=50)
-
-    def __str__(self):
-        return f"{self.buyer} - {self.price}"
