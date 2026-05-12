2a2e091258ed316aa6133da04b64c0d48ccc4870
einarberg <59967614+einarberg@users.noreply.github.com>
2026-05-12 13:21:12 +0000

    Update views.py

--- artwork/views.py
+++ artwork/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import get_object_or_404
 
 def all_artworks(request):
     artworks = Artwork.objects.all()
-    return render(request, "artwork/all_artworks.html", {"artwork": artworks})
+    return render(request, "artwork/all_artworks.html", {"artworks": artworks})
 
 def homepage(request):
     artworks = Artwork.objects.all()
