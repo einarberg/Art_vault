52d72465f5427e377cfcceee9710754ca96ad756
Freyrgrimsson <129167138+Freyrgrimsson@users.noreply.github.com>
2026-05-13 13:08:04 +0000

    Update models.py

--- finalize_bid/models.py
+++ finalize_bid/models.py
@@ -26,3 +26,25 @@ class Bid(models.Model):
 
     def __str__(self):
         return f"{self.buyer} - {self.price}"
+
+
+class Contact_information(models.Model):
+    national_id = CharField(max_length=100)
+
+    buyer = models.ForeignKey(
+        Buyer,
+        on_delete.models=CASCADE,
+        related_name='finalize_bid'
+    )
+
+    bid = models.ForeignKey(
+        Bid,
+        on_delete.models=CASCADE,
+        related_name='finalize_bid'
+    )
+
+    Street_name = CharField(max_length=100)
+    City = CharField(max_length=100)
+    Postal_code = CharField(max_length=100)
+    Country = CharField(max_length=100)
+
