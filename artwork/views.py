c63a4c3a4c9dc29fd31b21abf787d17363416ef9
einarberg <59967614+einarberg@users.noreply.github.com>
2026-05-14 12:59:20 +0000

    add js file for search and updated artwork views for search

--- artwork/views.py
+++ artwork/views.py
@@ -1,21 +1,33 @@
 from django.core.signals import request_started
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from psycopg2.extensions import JSON
+
+import artwork
 from artwork.models import Artwork
 from artwork.models import Style
 from django.shortcuts import get_object_or_404
 
 
+def homepage(request):
+    if 'search_filter' in request.GET:
+        return JsonResponse({
+            'data': [{
+                'id': x.id,
+                'thumbnail': x.thumbnail.url if x.thumbnail else None,
+                'title': x.title,
+                'artist': x.artist.seller.user.name,
+            } for x in Artwork.objects.filter(title__icontains=request.GET['search_filter']).order_by('title')]
+        })
+
+    artworks = Artwork.objects.all()
+    return render(request, "artwork/homepage.html", {"artwork": artworks})
 def all_artworks(request):
     artworks = Artwork.objects.all()
     return render(request, "artwork/all_artworks.html", {"artworks": artworks})
 def all_artists(request):
     artworks = Artwork.objects.all()
     return render(request, "artwork/all_artist.html", {"artworks": artworks})
-def homepage(request):
-    artworks = Artwork.objects.all()
-    return render(request, "artwork/homepage.html", {"artwork": artworks})
-    
 def get_artwork_by_id(request, id):
     artwork = get_object_or_404(Artwork, id=id)
     style = get_object_or_404(Style, id=artwork.style_id)
