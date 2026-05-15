40cfc86118378fdd66f4140a75a89b64c81c6497
einarberg <59967614+einarberg@users.noreply.github.com>
2026-05-15 13:18:56 +0000

    update search and homepage

--- static/js/artwork_filter_search.js
+++ static/js/artwork_filter_search.js
@@ -1,6 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
     function registerSearchButtonHandler(){
-        //
+        const searchButton = document.getElementById('search-icon');
+        searchButton.addEventListener('click',async function () {
+            const searchValueElement = document.getElementById('search-value');
+            const value = searchValueElement.value;
+            const artworkPlaceholder = document.getElementById('artwork-search');
+            const response = await fetch(`?search_filter=${value}`);
+
+            if(response.ok) {
+                const json = await response.json();
+                const artworks = json.data.map(artwork =>`
+                <div class="artwork-info">
+                    <a href="#">
+                        <div>
+                            <div class="artwork-thumbnail" style="background-image: url('${artwork.thumbnail}')"></div>
+                        </div>
+                        <h3>${artwork.title}</h3>
+                        <!--<p>{artwork.artist.seller.user.name}</p>-->
+                    </a>
+                </div>
+                
+                
+                `);
+
+                artworkPlaceholder.innerHTML = artworks.join('');
+            }
+        });
     }
 
     registerSearchButtonHandler();
