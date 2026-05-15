27c7a73ccf816dddd0e231420f0e29d372c80588
Freyrgrimsson <129167138+Freyrgrimsson@users.noreply.github.com>
2026-05-15 10:00:44 +0000

    Add edit profile path to user URLs

--- user/urls.py
+++ user/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='Users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page='homepage'), name='logout'),
     path('profile/', views.profile, name='profile'),
+    path('profile/edit/', views.edit_profile, name='edit_profile'),
 ]
