6ab44fa9f2d0c299b1612afba113565f62bf8cfb
Freyrgrimsson <129167138+Freyrgrimsson@users.noreply.github.com>
2026-05-15 10:57:06 +0000

    Update profile URL to include user ID

--- user/urls.py
+++ user/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name='Users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page='homepage'), name='logout'),
-    path('profile/', views.profile, name='profile'),
+    path('profile/<int:id>/', views.profile, name='profile'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
 ]
