389023d25b48a8d34b42dea82aabf0a98b6b48d5
Domstjorinn <bjarkisaem@gmail.com>
2026-05-16 19:42:25 +0000

    Implemented finalize bids UI flow and payment navigation

--- finalize_bid/urls.py
+++ finalize_bid/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='finalize-bid-index'),
+    path('payment/', views.payment, name='finalize-bid-payment'),
+    path('payment/credit/', views.payment, name='credit-card'),
+    path('payment/bank/', views.bank_transfer, name='bank-transfer'),
+    path('payment/wire/', views.wire_transfer, name='wire-transfer'),
     path('bids/<int:id>/', views.index, name='get_bids'),
+    path('review/', views.review, name='finalize-bid-review'),
 ]
--- finalize_bid/views.py
+++ finalize_bid/views.py
@@ -8,9 +8,18 @@ def index(request):
     return render(request, "finalize_bid/finalize_bid.html", {
         "artwork": None
     })
+def payment(request):
+    return render(request, "finalize_bid/finalize_bid_payment.html")
 
+def bank_transfer(request):
+    return render(request, "finalize_bid/finalize_bid_bank.html")
+
+def wire_transfer(request):
+    return render(request, "finalize_bid/finalize_bid_wire.html")
 
 def get_bids(request, id):
     bids = Bid.objects.filter(buyer__user__id=id)
     return render(request, "finalize_bid/bids.html", {"bids": bids})
 
+def review(request):
+    return render(request, "finalize_bid/finalize_bid_review.html")
