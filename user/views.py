0fef8c0a454fc6979daadef12e75e00e1e0d0964
Freyrgrimsson <129167138+Freyrgrimsson@users.noreply.github.com>
2026-05-16 11:50:03 +0000

    Update views.py

--- user/views.py
+++ user/views.py
@@ -2,6 +2,7 @@ from user.forms import ProfileForm
 from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
 from user.models import User
+from artwork.models import Artwork
 from user.forms import CustomUserCreationForm
 
 
