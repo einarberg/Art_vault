0b2b12ff4a3b4fdf7a1f40d0c2c365856a1bc0e4
Freyrgrimsson <129167138+Freyrgrimsson@users.noreply.github.com>
2026-05-16 13:27:30 +0000

    Update urls.py

--- artwork/urls.py
+++ artwork/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('all_artworks/', views.all_artworks, name='all-artworks'),
     path('all_artist/', views.all_artists, name='all-artists'),
+    path('artist/<int:id>', views.artist_artworks, name='artist-artworks'),
     path('<int:id>', views.get_artwork_by_id, name='artwork-by-id'),
 ]
