9309e3930bddbadc2152647ebf376c85c1dea50d
Freyrgrimsson <129167138+Freyrgrimsson@users.noreply.github.com>
2026-05-16 11:14:46 +0000

    Update urls.py

--- user/urls.py
+++ user/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='Users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page='homepage'), name='logout'),
     path('profile/<int:id>/', views.profile, name='profile'),
+    path('seller_profile/<int:id>/', views.seller_profile, name='seller_profile'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
 ]
