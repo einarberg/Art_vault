2fb6f7d50858a04e0102cf036c30204b5be23455
einarberg <59967614+einarberg@users.noreply.github.com>
2026-05-16 14:30:36 +0000

    updated artwork models

--- artwork/models.py
+++ artwork/models.py
@@ -48,6 +48,8 @@ class Artwork(models.Model):
 
     bid_status = models.CharField(max_length=50)
 
+    start_bid_price = models.CharField(max_length=200)
+
     def __str__(self):
         return self.title
 
